a2f80f8bd7403ba108f5454efbeccbc75a52e037
rohan2734 <jagandevaki1@gmail.com>
2021-01-26 23:28:24 +0530

    added containers and custom styles

--- projects/quiz_app/lib/question.dart
+++ projects/quiz_app/lib/question.dart
@@ -1,13 +1,24 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 //st
 class Question extends StatelessWidget {
-  String questionText;
+  final String questionText; //for making this immutable, we added a final here
 
-  Question(this.questionText);//constructor, it is not a named argument, it is positional argument, if it is wrapped with curly bracket, it is then named arugment
+  Question(
+      this.questionText); //constructor, it is not a named argument, it is positional argument, if it is wrapped with curly bracket, it is then named arugment
 
   @override
   Widget build(BuildContext context) {
-    return Text(questionText);
+    return Container(
+      width: double.infinity,//as much width as it can get, 
+      margin: ,//spacing around the container
+      child:Text(
+        questionText,
+        style: TextStyle(fontSize: 28),
+        textAlign: TextAlign.center,
+    ),
+    );
   }
 }
